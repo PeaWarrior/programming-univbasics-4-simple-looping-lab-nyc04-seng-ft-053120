b599a6c375769a22d7b88a38e41db59bc292fd32
Jackson Chen <jacksonchen171@gmail.com>
2020-06-04 16:53:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,11 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, int)
+  int.times do
+    puts string
+  end
+end
